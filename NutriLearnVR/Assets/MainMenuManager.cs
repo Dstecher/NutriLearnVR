d22573482dbb46a148d91befe8d90772ece1c40e
Dominik <dstecher@mail.uni-paderborn.de>
2023-09-06 23:23:11 +0200

    added MainMenuManager to always display canvas in front of user; binded action to Y button

---
+++ NutriLearnVR/Assets/MainMenuManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] public Transform head;
+    [SerializeField] public float menuDistance = 2;
+    [SerializeField] public GameObject menuCanvas;
+    [SerializeField] public InputActionProperty showButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (showButton.action.WasPressedThisFrame())
+        {
+            Debug.Log("The button was pressed");
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+
+            
+        }
+
+        menuCanvas.transform.position = head.position + new Vector3(head.forward.x, head.forward.y, head.forward.z).normalized * menuDistance;
+        menuCanvas.transform.LookAt(new Vector3(head.position.x, head.position.y, head.position.z));
+        menuCanvas.transform.forward *= -1;
+    }
+}
