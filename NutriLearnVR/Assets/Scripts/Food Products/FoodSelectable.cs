2dd0a9db3b329061fa0201dc196ad20ce895880c
Dominik <dstecher@mail.uni-paderborn.de>
2023-09-16 00:14:18 +0200

    migrated project to newer Unity version, tried to fix .csproj

--- NutriLearnVR/Assets/Scripts/Food Products/FoodSelectable.cs
+++ NutriLearnVR/Assets/Scripts/Food Products/FoodSelectable.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class FoodSelectable : MonoBehaviour
 {
 
     public GameObject selectionController;
 
-    private 
+    private XRGrabInteractable grabInteractable;
 
     // Start is called before the first frame update
     void Start()
