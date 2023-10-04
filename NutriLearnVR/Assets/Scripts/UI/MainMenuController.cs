6d7826917991a6ef79eb9a482a2c609d33cbd51f
Dominik <dstecher@mail.uni-paderborn.de>
2023-10-05 00:39:09 +0200

    adjusted UI back/close button description & functionality

--- NutriLearnVR/Assets/Scripts/UI/MainMenuController.cs
+++ NutriLearnVR/Assets/Scripts/UI/MainMenuController.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] GameObject currentCanvas;
     [SerializeField] GameObject mainContentUI;
     [SerializeField] GameObject controlsUI;
+    [SerializeField] private Text closeButtonText;
+
+    private GameObject currentlyActiveUI;
 
     public void CloseUI()
     {
-        if (currentCanvas) currentCanvas.SetActive(false);
+        if (currentlyActiveUI == mainContentUI)
+        {
+            if (currentCanvas) currentCanvas.SetActive(false);
+        }
+        else 
+        {
+            currentlyActiveUI.SetActive(false);
+            mainContentUI.SetActive(true);
+            currentlyActiveUI = mainContentUI;
+            closeButtonText.text = "←   schließen";
+        }
     }
 
     public void ShowControlsUI()
     {
         if (mainContentUI) mainContentUI.SetActive(false);
         if (controlsUI) controlsUI.SetActive(true);
+        currentlyActiveUI = controlsUI;
+        closeButtonText.text = "←   zurück";
     }
 
+    private void Awake() {
+        currentlyActiveUI = mainContentUI;
+    }
 
 }
