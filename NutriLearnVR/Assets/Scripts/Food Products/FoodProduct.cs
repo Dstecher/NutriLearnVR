209a1a30ac43a7af50e0de3a730f921a16e6d089
Dominik <dstecher@mail.uni-paderborn.de>
2023-11-27 21:15:41 +0100

    added new components from database in database reader

--- NutriLearnVR/Assets/Scripts/Food Products/FoodProduct.cs
+++ NutriLearnVR/Assets/Scripts/Food Products/FoodProduct.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class FoodProduct
 {
     public int id;
-        public string productName;
-        public int cals;
-        public float fats;
-        public float carbs;
-        public float protein;
-        public bool isLiquid;
+    public string productName;
+    public int cals;
+    public float fats;
+    public float carbs;
+    public float protein;
+    public bool isLiquid;
+    public float saturatedFat;
+    public float sugar;
+    public int nutriScore;
 }
