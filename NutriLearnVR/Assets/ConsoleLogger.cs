e9afb4e72db2572b7dbb174906923a628f14e204
Dominik <dstecher@mail.uni-paderborn.de>
2023-12-02 22:30:16 +0100

    latest update to ConsoleLogger with logging on custom webserver

--- NutriLearnVR/Assets/ConsoleLogger.cs
+++ NutriLearnVR/Assets/ConsoleLogger.cs
@@ -3,42 +3,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 
 public class ConsoleLogger : MonoBehaviour
 {
-    private string participantID;
-    private string filePath;
-    private string fileName;
- 
-    private void Start()
+
+    private string sendString = "";
+
+    void Start()
     {
-        fileName = "userLogs" + Time.time;
-        participantID = PlayerPrefs.GetString("ID", "INVALID");
-        filePath = GetFilePath();
     }
- 
-    private void Update()
+
+    public void NextTry()
     {
-        AddRecord("test");
+        string URL3 = "https://www.sse-testcenter.org/unity/upload";
+        
+        WebRequest requestS3 = (HttpWebRequest) WebRequest.Create(URL3);
+
+        byte[] fileRawBytes = System.Text.Encoding.UTF8.GetBytes(sendString);
+        Debug.Log($"Packed the following string: {sendString}");
+        requestS3.ContentLength = fileRawBytes.Length;
+
+        requestS3.Method = "PUT";
+
+        Stream S3Stream = requestS3.GetRequestStream();
+        S3Stream.Write(fileRawBytes, 0, fileRawBytes.Length);
+
+        Debug.Log("Sent Message to SSE");
+
+        WebResponse myResponse = requestS3.GetResponse();
+
+        Debug.Log(myResponse.ToString());
+        
+        S3Stream.Close();
+
+        Debug.Log("closed s3 stream");
     }
- 
-    public void AddRecord(string logMessage)
+
+    
+
+    void Update()
     {
-        try
-        {
-            using (StreamWriter file = new StreamWriter(@filePath, false))
-            {
-                file.WriteLine($"{Time.time}: {logMessage} \n");
-            }
-        }
-        catch (Exception ex)
-        {
-            Debug.Log("Something went wrong! Error: " + ex.Message);
-        }
+        AppendToSendString("test" + System.DateTime.UtcNow.ToString() + "\n");
     }
- 
-    string GetFilePath()
+
+    public void AppendToSendString(string appendString)
     {
-        return Application.persistentDataPath + "/" + fileName + ".txt";
+        sendString += appendString;
     }
 }
